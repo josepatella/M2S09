50d986c1d4d03b323cf9f606bac6d9811c2c0f52
Patella <jfpatella@gmail.com>
2023-07-31 20:59:19 -0300

    Classe Pessoa

---
+++ src/main/java/entidades/Pessoa.java
@@ -0,0 +1,40 @@
+package entidades;
+
+public class Pessoa {
+    private Long Id;
+    private String nome;
+    private String email;
+    private Boolean status;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
